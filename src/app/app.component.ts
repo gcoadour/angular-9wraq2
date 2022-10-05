869fae1fae071225aaa91483ce588a9172955892
gcoadour <47463874+gcoadour@users.noreply.github.com>
2022-10-05 16:59:34 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,118 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name = 'Angular';
-  hoursColor = [
-    1, 2, 3, 1, 1, 1, 1, 1, 1, 1, 1, 2, 3, 2, 1, 3, 2, 1, 2, 1, 1, 1, 1, 1, 1,
+  values = [
+    {
+      pas: 0,
+      hvalue: 1,
+    },
+    {
+      pas: 1,
+      hvalue: 2,
+    },
+    {
+      pas: 2,
+      hvalue: 3,
+    },
+    {
+      pas: 3,
+      hvalue: 1,
+    },
+    {
+      pas: 4,
+      hvalue: 1,
+    },
+    {
+      pas: 5,
+      hvalue: 1,
+    },
+    {
+      pas: 6,
+      hvalue: 2,
+    },
+    {
+      pas: 7,
+      hvalue: 2,
+    },
+
+    {
+      pas: 8,
+      hvalue: 3,
+    },
+
+    {
+      pas: 9,
+      hvalue: 1,
+    },
+
+    {
+      pas: 10,
+      hvalue: 1,
+    },
+
+    {
+      pas: 11,
+      hvalue: 1,
+    },
+
+    {
+      pas: 12,
+      hvalue: 1,
+    },
+
+    {
+      pas: 13,
+      hvalue: 1,
+    },
+
+    {
+      pas: 14,
+      hvalue: 1,
+    },
+
+    {
+      pas: 15,
+      hvalue: 1,
+    },
+
+    {
+      pas: 16,
+      hvalue: 1,
+    },
+
+    {
+      pas: 17,
+      hvalue: 1,
+    },
+
+    {
+      pas: 18,
+      hvalue: 1,
+    },
+
+    {
+      pas: 19,
+      hvalue: 1,
+    },
+
+    {
+      pas: 20,
+      hvalue: 1,
+    },
+
+    {
+      pas: 21,
+      hvalue: 1,
+    },
+
+    {
+      pas: 22,
+      hvalue: 1,
+    },
+
+    {
+      pas: 23,
+      hvalue: 1,
+    },
   ];
 }
