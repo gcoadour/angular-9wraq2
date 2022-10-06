46c94d4ca6b47c1201e784e54f4ddace0e6e46bd
gcoadour <47463874+gcoadour@users.noreply.github.com>
2022-10-06 16:40:58 +0200

    1 file modified, 3 added

---
+++ src/app/energy-sobriety/energy-sobriety-details/energy-sobriety-details.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'energy-sobriety-details',
+  templateUrl: './energy-sobriety-details.component.html',
+  styleUrls: ['./energy-sobriety-details.component.css'],
+})
+export class EnergySobrietyDetailsComponent {}
