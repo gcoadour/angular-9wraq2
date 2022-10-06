3a84fc4b889c019f5db6c43825e9c77e8b7bf9ae
gcoadour <47463874+gcoadour@users.noreply.github.com>
2022-10-06 11:19:19 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent {
     },
     {
       pas: 1,
-      hvalue: 2,
+      hvalue: 3,
     },
     {
       pas: 2,
@@ -40,82 +40,66 @@ export class AppComponent {
       pas: 7,
       hvalue: 2,
     },
-
     {
       pas: 8,
       hvalue: 3,
     },
-
     {
       pas: 9,
       hvalue: 1,
     },
-
     {
       pas: 10,
       hvalue: 1,
     },
-
     {
       pas: 11,
       hvalue: 1,
     },
-
     {
       pas: 12,
       hvalue: 1,
     },
-
     {
       pas: 13,
       hvalue: 1,
     },
-
     {
       pas: 14,
       hvalue: 1,
     },
-
     {
       pas: 15,
       hvalue: 1,
     },
-
     {
       pas: 16,
       hvalue: 1,
     },
-
     {
       pas: 17,
       hvalue: 1,
     },
-
     {
       pas: 18,
       hvalue: 1,
     },
-
     {
       pas: 19,
       hvalue: 1,
     },
-
     {
       pas: 20,
       hvalue: 1,
     },
-
     {
       pas: 21,
       hvalue: 1,
     },
-
     {
       pas: 22,
       hvalue: 1,
     },
-
     {
       pas: 23,
       hvalue: 1,
